09883d7b73c6c81df609d47c6aa488091b1c171b
hussain <m.hussainul.islam@gmail.com>
2019-08-24 22:54:34 -0400

    addition: error check if word not found

--- dictionary.py
+++ dictionary.py
@@ -1,11 +1,15 @@
 import json
 
-
-
 def loadData():
   return json.load(open('data.json'))
 
-
+def definition(dict, word):
+  retValue = ''
+  if word in dict:
+    retValue = dict[word]
+  else:
+    retValue = 'This word doesn\'t exist. Please check the word.'
+  return retValue
 
 def main():
   data = loadData()
@@ -15,7 +19,7 @@ def main():
     if word == ':exit':
       break
     else:
-      print(data[word])
+      print(definition(data, word))
 
 
 if __name__ == '__main__': main()
