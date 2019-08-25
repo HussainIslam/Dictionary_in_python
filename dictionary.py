cf07b47045dd597c092264082ca547a918586817
hussain <m.hussainul.islam@gmail.com>
2019-08-24 23:30:08 -0400

    addition: alternative words menu

--- dictionary.py
+++ dictionary.py
@@ -1,14 +1,28 @@
 import json
+import traceback
+import difflib
 
 def loadData():
   return json.load(open('data.json'))
+
+def closeMatchMenu(wordList):
+  counter = 1
+  for i in wordList:
+    print(f'{counter}: {i}')
+    counter = counter + 1
 
 def definition(dict, word):
   retValue = ''
   try:
     retValue = dict[word.lower()]
+  except KeyError:
+    try:
+      closeMatches = difflib.get_close_matches(word, dict.keys())
+      closeMatchMenu(closeMatches)
+    except:
+      retValue = '** ERROR: This word doesn\'t exist. Please check the word. **'
   except:
-    retValue = '** ERROR: This word doesn\'t exist. Please check the word. **'
+    print("Unexpected error.")
   return retValue
 
 def main():
